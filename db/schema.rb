aee4d5e6f0533a8ae86119513eba3637c64d7f59
ilya-sh <ilyash.on5lab@gmail.com>
2018-08-18 15:02:49 +0300

    Revert "home"
    This reverts commit a166a8d3f93ab1b9df940dba6e6c8e46c109f264.

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2018_08_10_141442) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
+  create_table "average_caches", force: :cascade do |t|
+    t.float "avg", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "rater_id", null: false
+    t.string "rateable_type", null: false
+    t.bigint "rateable_id", null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_average_caches_on_rateable_type_and_rateable_id"
+    t.index ["rater_id"], name: "index_average_caches_on_rater_id"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -33,6 +44,38 @@ ActiveRecord::Schema.define(version: 2018_08_10_141442) do
     t.bigint "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "overall_averages", force: :cascade do |t|
+    t.float "overall_avg", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "rateable_type", null: false
+    t.bigint "rateable_id", null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_overall_averages_on_rateable_type_and_rateable_id"
+  end
+
+  create_table "rates", force: :cascade do |t|
+    t.float "stars", null: false
+    t.string "dimension"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "rater_id", null: false
+    t.string "rateable_type", null: false
+    t.bigint "rateable_id", null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_rates_on_rateable_type_and_rateable_id"
+    t.index ["rater_id"], name: "index_rates_on_rater_id"
+  end
+
+  create_table "rating_caches", force: :cascade do |t|
+    t.float "avg", null: false
+    t.integer "qty", null: false
+    t.string "dimension"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "cacheable_type"
+    t.bigint "cacheable_id"
+    t.index ["cacheable_type", "cacheable_id"], name: "index_rating_caches_on_cacheable_type_and_cacheable_id"
   end
 
   create_table "users", force: :cascade do |t|
