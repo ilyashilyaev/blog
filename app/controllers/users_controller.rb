ef34935ce868150175081f32a300ba134bc42cb9
ilya-sh <ilyash.on5lab@gmail.com>
2018-08-29 01:03:51 +0300

    home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,25 @@ class UsersController < ApplicationController
 
     def edit
       @user = User.find(params[:id])
+      end
+
+
+    def show
+      @user = User.find(params[:id])
     end
 
-  def show
-    @user = User.find(params[:id])
-  end
+    def new
 
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
 
     private
 
